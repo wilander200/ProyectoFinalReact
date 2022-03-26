9d0bdcdd528d692b6ef50867fac8ea26346cef78
Wilander Velazco <wilandervelazco@gmail.com>
2022-03-26 20:47:11 -0300

    inicio del desafio 9

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZSLr3ah400OxNxCEVAKdiS35qiu5vbIQ",
+  authDomain: "gustavovelazcods.firebaseapp.com",
+  projectId: "gustavovelazcods",
+  storageBucket: "gustavovelazcods.appspot.com",
+  messagingSenderId: "294115364608",
+  appId: "1:294115364608:web:1fc7dea323b5dc5c00ade5"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const getFireStoreApp = () => {
+    return app
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FirebaseApp } from 'firebase/app';
 
+//getFireStoreApp
 
 ReactDOM.render(
   <React.StrictMode>
