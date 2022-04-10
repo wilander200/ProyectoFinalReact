e782db941ab3408be0fc22f0b6574be55373192a
Wilander Velazco <wilandervelazco@gmail.com>
2022-04-10 00:39:28 -0400

    Terminando desafio clase 13

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,7 +9,7 @@ export default function Cart() {
   const {cartList, erraseCart, erraseItem, totalPrice} = useContext(CartContext)
   const [id, setId] = useState(null)
   const [pay , setPay] = useState (true)
-  const [dataForm, setDataForm] = useState({name:'', email:'', phone:'', address:''})
+  const [dataForm, setDataForm] = useState({name:'', email:'', phone:'', address:'', rEmail: ' '})
 
   function newOrder (e) {
     e.preventDefault()
@@ -88,16 +88,18 @@ export default function Cart() {
 
             { pay &&
             <div className="col-3 border border-secondary rounded">
-              <form className="m-3" onSubmit={newOrder}>
+              <form className="m-2" onSubmit={newOrder}>
                 <label for="name" class="form-label m-1 fw-bold">Ingrese nombre y apellido:</label>
-                <input className="m-2" type='text' name="name" placeholder="Nombre" value={dataForm.name} onChange={handleChange}/>
+                <input className="m-1" type='text' name="name" placeholder="Nombre" value={dataForm.name} onChange={handleChange}/>
                 <label for="phone" class="form-label m-1 fw-bold">Ingrese número de teléfono:</label>
-                <input className="m-2" type='text' name="phone" placeholder="Telefono" value={dataForm.phone} onChange={handleChange}/>
+                <input className="m-1" type='text' name="phone" placeholder="Telefono" value={dataForm.phone} onChange={handleChange}/>
                 <label for="address" class="form-label m-1 fw-bold">Ingrese su direccion:</label>
-                <input className="m-2" type='text' name="address" placeholder="Direccion" value={dataForm.address} onChange={handleChange}/>
+                <input className="m-1" type='text' name="address" placeholder="Direccion" value={dataForm.address} onChange={handleChange}/>
                 <label for="email" class="form-label m-1 fw-bold">Ingrese su correo electronico:</label>
-                <input className="m-2" type='email' name="email" placeholder="e-mail" value={dataForm.email} onChange={handleChange}/>
-                <Button className="m-3" variant="outline-success" onClick={newOrder}>Generar Orden</Button>
+                <input className="m-1" type='email' name="email" placeholder="e-mail" value={dataForm.email} onChange={handleChange}/>
+                <label for="rEmail" class="form-label m-1 fw-bold">Repita su correo electronico:</label>
+                <input className="m-1" type='email' name="rEmail" placeholder="repita e-mail" value={dataForm.rEmail} onChange={handleChange}/>
+                <Button disabled= {dataForm.email!=dataForm.rEmail} className="m-2" variant="outline-success" onClick={newOrder}>Generar Orden</Button>
               </form>
             </div>
             }
@@ -107,7 +109,7 @@ export default function Cart() {
             <div>
             <h3 className="d-flex justify-content-end"> {`Monto total de la compra : ${totalPrice().toLocaleString("es-CL")} CLP`}</h3>
             <Link to = '/'>
-            <Button className="m-3" variant="outline-success" onClick={erraseCart}>Pagar</Button>
+            <Button className="m-3" variant="outline-success" onClick={erraseCart}>Confirmar Pedido</Button>
             </Link>
             </div>
             :
